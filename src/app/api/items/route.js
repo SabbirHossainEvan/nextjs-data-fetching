7bc766c5d16c1d1f2011aa7558d9b0ca19099dea
SabbirHossainEvan <evansabbir31@gmail.com>
2025-10-30 19:37:48 +0600

    2nd commit

---
+++ src/app/api/items/route.js
@@ -0,0 +1,10 @@
+export async function GET() {
+  
+    const data = {
+        message: "successfully get data",
+        error: false,
+        status: 200
+    }
+
+  return Response.json({ data })
+}
