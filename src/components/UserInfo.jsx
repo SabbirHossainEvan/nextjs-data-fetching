8d6fd14425aee4fd60d0b6821118afd4ffd4b5e7
SabbirHossainEvan <evansabbir31@gmail.com>
2025-11-01 19:49:24 +0600

    added

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -5,9 +5,7 @@ import { useSession } from "next-auth/react"
 export default function useInfo() {
     const session = useSession()
     return <div>
-        <p>
-           {Json.stringify(session)} 
-        </p>
+        <section></section>
 
     </div>
 }
