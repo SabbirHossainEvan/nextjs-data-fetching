3533344883bd0e34a2cc93c96089013a3029ec30
SabbirHossainEvan <evansabbir31@gmail.com>
2025-11-01 19:44:34 +0600

    added

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,13 +1,13 @@
-"use client"
+// "use client"
 
-import { useSession } from "next-auth/react"
+// import { useSession } from "next-auth/react"
 
-export default function useInfo() {
-    const session = useSession()
-    return <div>
-        <p>
-            {Json.stringify(session)}
-        </p>
+// export default function useInfo() {
+//     const session = useSession()
+//     return <div>
+//         <p>
+//             {Json.stringify(session)}
+//         </p>
 
-    </div>
-}
+//     </div>
+// }
